0579894f009eed97558d916881fcf57fc30bbd22
Regan Barua <reganbarua2015@gmail.com>
2021-03-26 00:26:31 -0700

    get method updated

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Thankyou for calling me");
+  res.send("Thankyou for calling me at 4200");
 });
 
-app.listen(3000, () => console.log("Listening to PORT 3000"));
+const users = ["Rahul", "Rohit", "Virat", "Gill", "ishan"];
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  res.send({ name: users[userId], id: userId });
+});
+
+app.listen(4200, () => console.log("Listening to PORT 4200"));
